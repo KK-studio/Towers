1c9932850b02ed3eff46d84223994c580aa287e8
kash <amkkashani@gmail.com>
2020-06-15 20:35:57 +0430

    gameManage is aproximately ready

---
+++ Assets/Scriptes/Aim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    // must setup direction in testing maybe angels dont works correctly
+    private Vector2 front = new Vector2(1, 0); 
+    public Vector2 direction;
+    public float power = 0;
+    public bool active = false;
+
+    public GameObject flesh;
+
+    // Start is called before the first frame update
+    public void setup(Vector2 direction,float power , bool active)
+    {
+        this.direction = direction;
+        this.power = power;
+        this.active = active;
+    }
+
+
+    private void Start()
+    {
+        flesh.SetActive(false);
+    }
+}
---
+++ Assets/Scriptes/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System;
+
+public class GameManager : MonoSingleton<GameManager>
+{
+    public int playerNumbers = 2;
+    public int towersNumber = 3;
+    public static int gameState = 0;
+    private int[] points;
+    private Action<int> drop;
+
+    private void shoot()
+    {
+        gameState = (gameState + 1) % playerNumbers;
+        drop += droped;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = Enumerable.Repeat(towersNumber,playerNumbers ).ToArray(); //make first points for players  size palyerNumbers   amount : numTower 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void droped(int playerID)
+    {
+        //you must find the team first :)
+        points[playerID]--;
+
+    }
+}
