78a3e8cce066f4bd1012cbf9d42ebbec4f6b0a0e
kash <amkkashani@gmail.com>
2020-06-15 21:42:30 +0430

    arrow is ready

--- Assets/Scriptes/Aim.cs
+++ Assets/Scriptes/Aim.cs
@@ -5,24 +5,47 @@ using UnityEngine;
 public class Aim : MonoBehaviour
 {
     // must setup direction in testing maybe angels dont works correctly
-    private Vector2 front = new Vector2(1, 0); 
-    public Vector2 direction;
+    private Vector2 front = new Vector2(1, 0);
+
+    public Vector3 minmumSize;
+    public Vector2 directionVector;
     public float power = 0;
     public bool active = false;
+    public bool testing = false; // you must turn this off when you want to use it in game
 
     public GameObject flesh;
 
-    // Start is called before the first frame update
-    public void setup(Vector2 direction,float power , bool active)
+    
+    public void setup(Vector2 directionVector,float power , bool active) 
     {
-        this.direction = direction;
+        flesh.SetActive(active);
+        float directionAngle;
+        this.directionVector = directionVector;
         this.power = power;
         this.active = active;
+        directionAngle = Vector2.SignedAngle(Vector2.right, directionVector); //targe euler angle
+        power = Mathf.Clamp(power, 0, 1);
+        this.gameObject.transform.localScale = minmumSize + new Vector3(power,0,power); //setup new angle
+        this.gameObject.transform.eulerAngles = new Vector3(0, directionAngle, 0);//setup new scale
+
+
     }
 
-
-    private void Start()
+    private void Update()//just for testing never use
     {
-        flesh.SetActive(false);
+        if (testing)
+        {
+            flesh.SetActive(true);
+            float directionAngle;
+            directionAngle = Vector2.SignedAngle(Vector2.right, directionVector);
+            power = Mathf.Clamp(power, 0, 1);
+            this.gameObject.transform.localScale = minmumSize + new Vector3(power, 0, power);
+            this.gameObject.transform.eulerAngles = new Vector3(0, directionAngle, 0);
+        }
+    }
+
+    private void Start() 
+    {
+        flesh.SetActive(false);// disable ui at the start
     }
 }
