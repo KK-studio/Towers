0b864e8320627922be3c886838802777e8c7d189
kash <amkkashani@gmail.com>
2020-06-15 21:47:22 +0430

    intial of dead zone

---
+++ Assets/Scriptes/DropArea.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropArea : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        //we must detect the player name
+    }
+}
