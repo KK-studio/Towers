e5913a884137033dc08c656450919425bb5b9152
kash <amkkashani@gmail.com>
2020-06-17 00:20:19 +0430

    some changes in Aim system

--- Assets/Scriptes/Aim.cs
+++ Assets/Scriptes/Aim.cs
@@ -12,27 +12,42 @@ public class Aim : MonoBehaviour
     public Vector2 directionVector;
     public float power = 0;
     public bool active = false;
-    public bool testing = false; // you must turn this off when you want to use it in game
+
+    private Transform target;
+    [SerializeField] private Vector3 strechScale = new Vector3(1, 0, 0.3f); // just x and z is important
+    [SerializeField]private bool testing = false; // you must turn this off when you want to use it in game
+    [SerializeField]private float maxPower =3;
 
     public GameObject flesh;
 
-    
+    public void setTargetTransform(Transform goal) // dan u must call this  :)
+    {
+        this.target = goal;
+    }
+
+
     public void setup(Vector2 startPos,Vector2 endPos) 
     {
         flesh.SetActive(true);
+        this.transform.position = target.position; // go sprite in to the target place you must set it before setup phase
         float directionAngle;
-        directionVector = directionVector = (endPos - startPos).normalized;
-        float Stretched = Vector3.Distance(endPos ,startPos);//TODO @kash check
+        directionVector = (endPos - startPos).normalized;
+        float Stretched = Vector3.Distance(endPos ,startPos);
         this.active = true;
         directionAngle = Vector2.SignedAngle(Vector2.right, directionVector); //targe euler angle
-       // power = Mathf.Clamp(power, 0, 1);TODO @kash check
-        this.gameObject.transform.localScale = minmumSize + new Vector3(Stretched,0,Stretched/2f); //setup new angle
+        power = Mathf.Clamp(Stretched, 0, maxPower); 
+        this.gameObject.transform.localScale = minmumSize + new Vector3(Stretched*strechScale.x,0,Stretched*strechScale.z); //setup new angle
         this.gameObject.transform.eulerAngles = new Vector3(0, directionAngle, 0);//setup new scale
     }
 
     public void disableFlesh()
     {
         flesh.SetActive(false);
+    }
+
+    public float currentPower()
+    {
+        return power;
     }
 #if UNITY_EDITOR
     private void Update()//just for testing never use
